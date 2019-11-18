3ed5a23216a47056c5f612acfb934fcaee26bcd4
rameznahas <rameznahas@gmail.com>
2019-11-18 13:30:20 -0500

    Refactoring

--- Assignment 3/floyd_bcast.cpp
+++ Assignment 3/floyd_bcast.cpp
@@ -84,6 +84,27 @@ void cpy_directed_graph(const int* G, int*& G_cpy, int total_count) {
 		G_cpy[i] = G[i];
 }
 
+void communication(int& row_buff, int*& col_buff, int*& g, int p_row, int p_col, int k, int count) {
+	MPI_Comm row_comm, col_comm;
+
+	int row_key = p_col == (k / count) ? 0 : 1;
+	int col_key = p_row == k ? 0 : 1;
+
+	MPI_Comm_split(MPI_COMM_WORLD, p_row, row_key, &row_comm);
+	MPI_Comm_split(MPI_COMM_WORLD, p_col, col_key, &col_comm);
+
+	if (row_key == 0) {
+		row_buff = g[k % count];
+	}
+
+	if (col_key == 0) {
+		for (int i = 0; i < count; ++i)
+			col_buff[i] = g[i];
+	}
+	MPI_Bcast(&row_buff, 1, MPI_INT, 0, row_comm);
+	MPI_Bcast(col_buff, count, MPI_INT, 0, col_comm);
+}
+
 void serial_floyds(int*& G, int n) {
 	for (int k = 0; k < n; ++k)
 		for (int row = 0; row < n; ++row)
@@ -96,6 +117,22 @@ void serial_floyds(int*& G, int n) {
 				int cost = G[idx_k_col] + G[idx_k_row];
 				G[idx] = cur_cost < cost ? cur_cost : cost;
 			}
+}
+
+void parallel_floyds_bcast(int*& g, int p_row, int p_col, int n, int count) {
+	for (int k = 0; k < n; ++k) {
+		int row_buff;
+		int* col_buff = new int[count];
+
+		communication(row_buff, col_buff, g, p_row, p_col, k, count);
+
+		for (int i = 0; i < count; ++i) {
+			int cur_cost = g[i];
+			int cost = row_buff + col_buff[i];
+			g[i] = cur_cost < cost ? cur_cost : cost;
+		}
+		delete[] col_buff;
+	}
 }
 
 void init(int argc, char** argv, int*& G, int*& G_cpy, int*& g, int& p, int& pid, int& n, int& total_count, int& count) {
@@ -132,27 +169,6 @@ void init(int argc, char** argv, int*& G, int*& G_cpy, int*& g, int& p, int& pid
 	}
 }
 
-void communication(int& row_buff, int*& col_buff, int*& g, int p_row, int p_col, int k, int count) {
-	MPI_Comm row_comm, col_comm;
-
-	int row_key = p_col == (k / count) ? 0 : 1;
-	int col_key = p_row == k ? 0 : 1;
-
-	MPI_Comm_split(MPI_COMM_WORLD, p_row, row_key, &row_comm);
-	MPI_Comm_split(MPI_COMM_WORLD, p_col, col_key, &col_comm);
-
-	if (row_key == 0) {
-		row_buff = g[k % count];
-	}
-
-	if (col_key == 0) {
-		for (int i = 0; i < count; ++i)
-			col_buff[i] = g[i];
-	}
-	MPI_Bcast(&row_buff, 1, MPI_INT, 0, row_comm);
-	MPI_Bcast(col_buff, count, MPI_INT, 0, col_comm);
-}
-
 int main(int argc, char** argv) {
 	int p, pid, n, total_count, count;
 	int* G = nullptr, *G_cpy = nullptr, *g = nullptr;
@@ -163,21 +179,7 @@ int main(int argc, char** argv) {
 	std::pair<int, int> p_coord = { pid / p_grid_width, pid % p_grid_width };
 
 	clock_t start = clock();
-
-	for (int k = 0; k < n; ++k) {
-		int row_buff;
-		int* col_buff = new int[count];
-
-		communication(row_buff, col_buff, g, p_coord.first, p_coord.second, k, count);
-		
-		for (int i = 0; i < count; ++i) {
-			int cur_cost = g[i];
-			int cost = row_buff + col_buff[i];
-			g[i] = cur_cost < cost ? cur_cost : cost;
-		}
-		delete[] col_buff;
-	}
-
+	parallel_floyds_bcast(g, p_coord.first, p_coord.second, n, count);
 	MPI_Barrier(MPI_COMM_WORLD);
 
 	if (pid == 0) {
